63301d14c13e664e0d437862575be2cd21606327
Firren <95839148+Firen-Setiaputra@users.noreply.github.com>
2024-10-22 20:49:27 +0700

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'category_id',
+        'image',
+        'title',
+        'content'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
